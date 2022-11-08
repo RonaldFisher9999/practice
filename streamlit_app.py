05d5df967f248cf9f90d2cc8c5132cfb6e3ea8ae
RonaldFisher9999 <1993sabre@gmail.com>
2022-11-08 18:30:14 +0900

    deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,24 +8,24 @@ st.write("# 보험료 예측")
 # 첫번째 행
 r1_col1, r1_col2, r1_col3 = st.columns(3)
 
-age = r1_col1.number_input("age (between 1~100)", step=1, value=20, min_value=1, max_value=100)
+age = r1_col1.number_input("나이 (1부터 100까지)", step=1, value=20, min_value=1, max_value=100)
 
-height = r1_col2.number_input("height (cm)", value=170)
-weight = r1_col2.number_input("weight (kg)", value=60)
+height = r1_col2.number_input("키 (cm)", step=None, value=170)
+weight = r1_col2.number_input("몸무게 (kg)", step=None, value=60)
 bmi = (height/100)**2 / weight
 
-children = r1_col3.number_input("children", step=1, value=0, min_value=0)
+children = r1_col3.number_input("자녀 수", step=1, value=0, min_value=0)
 
 # 두번째 행
 r2_col1, r2_col2, r2_col3 = st.columns(3)
 
-smoker_option = r2_col1.radio(label='smoker', options=("yes", "no"))
-if smoker_option == "yes" :
+smoker_option = r2_col1.radio(label='흡연 여부', options=("예", "아니오"))
+if smoker_option == "예" :
     smoker = 1
 else :
     smoker = 0
 
-sex_option = r2_col2.radio(label='sex', options=("male", "female"))
+sex_option = r2_col2.radio(label='성별', options=("남성", "여성"))
 if sex_option == "male" :
     sex = 1
 else :
